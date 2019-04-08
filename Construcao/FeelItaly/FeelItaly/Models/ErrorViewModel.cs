7e7a76c38be60fae50c513b40ec59cd30885770f
joaop21 <jpsilva9898@gmail.com>
2019-04-08 18:47:28 +0100

    novo projeto MVC

---
+++ Construcao/FeelItaly/FeelItaly/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FeelItaly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
