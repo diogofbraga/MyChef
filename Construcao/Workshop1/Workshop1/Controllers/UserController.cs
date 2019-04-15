26cf715ce98b6644d588aab4b591543845c57ffc
joaop21 <jpsilva9898@gmail.com>
2019-04-15 18:44:52 +0100

    workshop para auxílio

---
+++ Construcao/Workshop1/Workshop1/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Workshop1.Models;
+
+namespace Workshop1.Controllers{
+
+    [Route("api/[controller]")]
+    public class UserController : Controller{
+
+        [HttpGet]
+        public User[] Get(){
+
+            return new[] {
+                    new User() {codigo = 1, nome = "Ugo", email = "teste@gmail.com"}, 
+                    new User() {codigo = 2, nome = "Simone", email= "Chiaretta@gmail.com"} 
+                    };
+        }
+
+        [HttpGet("{codigo}")]
+        public User Get(int codigo){
+
+            var users = new[] {
+                    new User() {codigo = 1, nome = "Ugo", email = "Lattanzi@gmail.com"}, 
+                    new User() {codigo = 2, nome = "Simone", email = "Chiaretta@gmail.com"} 
+                    };
+
+            return users.FirstOrDefault(x => x.codigo == codigo);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] User user){
+
+            var users = new List<User>();
+            users.Add(user);
+            return new CreatedResult($"/api/user/{user.codigo}", user);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] int codigo){
+
+            var users = new[] {
+                        new User() {codigo = 1, nome = "Ugo", email = "Lattanzi@gmail.com"},
+                        new User() {codigo = 2, nome = "Simone", email = "Chiaretta@gmail.com"} 
+                        };
+
+            var user = users.SingleOrDefault(x => x.codigo == codigo);
+
+            if (user != null){
+                users.ToList().Remove(user);
+                return new EmptyResult();
+            }
+
+            return new NotFoundResult();
+        }
+    }
+}
---
+++ Construcao/Workshop1/Workshop1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshop1.Models{
+
+    public class User{
+
+        public int codigo { set; get; }
+        public string nome { set; get; }
+        public string email { set; get; }
+
+    }
+}
