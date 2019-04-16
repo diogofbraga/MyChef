1583e096fb452b1fb32fbbbb06af227cadb34855
unknown <rmilhazesveloso@gmail.com>
2019-04-16 17:18:19 +0100

    Tutorial e ...

--- Construcao/FeelItaly/FeelItaly/Models/ReceitaPasso.cs
+++ Construcao/FeelItaly/FeelItaly/Models/ReceitaPasso.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 namespace FeelItaly.Models{
 
     public class ReceitaPasso{
 
-        public Receita receita { set; get; }
-        public Passo passo { set; get; }
-        public int numero { set; get; }
+        [Key]
+        [Required]
+        public Receita Receita { set; get; }
+
+        [Key]
+        [Required]
+        public Passo Passo { set; get; }
+
+        [Required]
+        public int Numero { set; get; }
 
     }
 }
