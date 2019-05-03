bac62e87e7439ff65aed3c5cb8d7f202b81b6599
Diogo Braga <diogofiliperfbraga@gmail.com>
2019-05-03 16:59:01 +0100

    View getReceitas

---
+++ Construcao/FeelItaly/FeelItaly/Controllers/ReceitaViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FeelItaly.Models;
+using FeelItaly.shared;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FeelItaly.Controllers{
+
+    [Route("[controller]/[action]")]
+    public class ReceitaViewController : Controller{
+
+        private ReceitaHandling receitaHandling;
+
+        public ReceitaViewController(ReceitaContext context){
+            //_context = context;
+            receitaHandling = new ReceitaHandling(context);
+        }
+
+        // GET: /<controller>/
+        public IActionResult getReceitas(){
+            Receita[] recipes = receitaHandling.getReceitas();
+            return View(recipes);
+        }
+    }
+}
---
+++ Construcao/FeelItaly/FeelItaly/shared/ReceitaHandling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using FeelItaly.Models;
+
+namespace FeelItaly.shared{
+
+    public class ReceitaHandling{
+
+        private readonly ReceitaContext _context;
+
+        public ReceitaHandling(ReceitaContext context){
+            _context = context;
+        }
+
+        public Receita[] getReceitas(){
+            return _context.receita.ToArray();
+        }
+    }
+}
