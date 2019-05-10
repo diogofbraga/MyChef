38f2affa389b3cc35ff11b56d4beeea4161ae63b
Diogo Braga <diogofiliperfbraga@gmail.com>
2019-05-11 00:15:23 +0100

    Receitas da HomePage já são hiperligações para a própria receita (Carbonara)

--- Construcao/FeelItaly/FeelItaly/Controllers/HomeController.cs
+++ Construcao/FeelItaly/FeelItaly/Controllers/HomeController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FeelItaly.Models;
+using FeelItaly.shared;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FeelItaly.Controllers{
 
     public class HomeController : Controller{
 
+        private ReceitaHandling receitaHandling;
+
+        public HomeController(ReceitaContext receitacontext)
+        {
+            receitaHandling = new ReceitaHandling(receitacontext);
+        }
+
         public IActionResult Index(){
-            return View();
+            Receita[] recipes = receitaHandling.getReceitas();
+            return View(recipes);
         }
 
         public IActionResult AboutUs(){
