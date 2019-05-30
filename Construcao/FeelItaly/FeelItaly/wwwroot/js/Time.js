fc9da32194cac1ecf5fff3d0a3852b7f639a4e01
joaop21 <jpsilva9898@gmail.com>
2019-05-30 17:49:10 +0100

    Timers

---
+++ Construcao/FeelItaly/FeelItaly/wwwroot/js/Time.js
@@ -0,0 +1,16 @@
+﻿var beginning = Date.now();
+
+function starttime(){
+    var t = Date.now() - beginning;
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  
+    document.getElementById('h').innerHTML=hours;  
+    document.getElementById('m').innerHTML=minutes;  
+    document.getElementById('s').innerHTML=seconds;
+
+    var q = setTimeout(starttime, 500);
+}
+
+window.onload = starttime;
