f947dec7fe8ba11a8fe49b2ef2839a6618cefd10
joaop21 <jpsilva9898@gmail.com>
2019-06-02 03:36:35 +0100

    stats receitas realizadas

--- Construcao/FeelItaly/FeelItaly/Controllers/HistoricoViewController.cs
+++ Construcao/FeelItaly/FeelItaly/Controllers/HistoricoViewController.cs
@@ -13,17 +13,49 @@ namespace FeelItaly.Controllers
     public class HistoricoViewController : Controller
     {
         private HistoricoHandling historicoHandling;
+        private ReceitaHandling receitaHandling;
 
-        public HistoricoViewController(HistoricoContext context)
+        public HistoricoViewController(HistoricoContext hcontext, ReceitaContext rcontext)
         {
-            historicoHandling = new HistoricoHandling(context);
+            historicoHandling = new HistoricoHandling(hcontext);
+            receitaHandling = new ReceitaHandling(rcontext);
         }
 
         [HttpGet]
         public IActionResult GetHistorico(string username)
         {
             Historico[] his = historicoHandling.getHistorico(username);
-            return View(his);
+
+            var map = new Dictionary<int, int>();
+
+            foreach (Historico h in his) {
+                if (!map.ContainsKey(h.idReceita)) 
+                { 
+                    if(h.NrPasso == 1)
+                        map.Add(h.idReceita, 1); 
+                }
+                else
+                {
+                    if (h.NrPasso == 1)
+                        map[h.idReceita]++;
+                }
+            }
+
+            Dictionary<Receita, int> mapreceita = new Dictionary<Receita, int>();
+
+            Dictionary<int, int>.KeyCollection keyColl = map.Keys;
+            int count = 0;
+            foreach (int i in keyColl)
+            {
+                Receita r = receitaHandling.getReceita(i);
+                mapreceita.Add(r, map[i]);
+                count += map[i];
+            }
+
+            HistoricoStat hs = new HistoricoStat();
+            hs.num_receitas = mapreceita;
+            hs.total_realizadas = count;
+            return View(hs);
         }
     }
 }
---
+++ Construcao/FeelItaly/FeelItaly/Models/HistoricoStat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FeelItaly.Models
+{
+    public class HistoricoStat{
+
+        public Dictionary<Receita, int> num_receitas;
+        public int total_realizadas;
+
+    }
+}
