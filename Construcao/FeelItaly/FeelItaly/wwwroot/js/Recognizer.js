0a584df5e6193a146878b5cc0b95c92851cce871
unknown <rmilhazesveloso@gmail.com>
2019-06-03 00:19:20 +0100

    Recognizer mais rápido

--- Construcao/FeelItaly/FeelItaly/wwwroot/js/Recognizer.js
+++ Construcao/FeelItaly/FeelItaly/wwwroot/js/Recognizer.js
@@ -1,22 +1,31 @@
 ﻿var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+//var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var comandos = ['próximo', 'anterior', 'repetir'];
-var grammar = '#JSGF V1.0; grammar comandos; public <comando> = ' + comandos.join(' | ') + ' ;'
+//var comandos = ['próximo', 'anterior', 'repetir'];
+//var grammar = '#JSGF V1.0; grammar comandos; public <comando> = ' + comandos.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
-var speechRecognitionList = new SpeechGrammarList();
+//var speechRecognitionList = new SpeechGrammarList();
 
-speechRecognitionList.addFromString(grammar, 1);
+//speechRecognitionList.addFromString(grammar, 1);
 
-recognition.grammars = speechRecognitionList;
+//recognition.grammars = speechRecognitionList;
 recognition.continuous = true;
 recognition.lang = 'pt-PT';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-recognition.start();
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function reconhecimento() {
+    await sleep(5000);
+    recognition.start();
+}
+
+reconhecimento();
 
 recognition.onresult = function (event) {
     var last = event.results.length - 1;
